70511047f48b8073bdebb26a67756fe385a894fe
niallgallagher <72068866+niallgallagher@users.noreply.github.com>
2023-12-11 19:35:31 +0000

    Add files via upload
    uploading r

--- R/PlayerMatch.R
+++ R/PlayerMatch.R
@@ -208,19 +208,27 @@ Min <- c(0)
 df = data.frame(Player,Season,Date,Comp,Round,Venue,Result,Squad,Opponent,Start,Pos,Min)
 #names(df.game2014) <- c('Player', 'Season', 'Date', 'Day','Comp','Round','Venue','Result','Squad','Opponent','Start','Pos','Min')
 
-df = data.frame()
+df.game2014 = data.frame()
 #Game-By-Game Player Data 
 for (i in 1:length(avector2014$PlayerURL)) {
-  output = fb_player_match_logs(avector2014$PlayerURL[i],season_end_year = 2014, stat_type = 'summary')
-  
-  
-  df = bind_rows(df,output)
+  tryCatch({
+    output = fb_player_match_logs(avector2014$PlayerURL[i], season_end_year = 2014, stat_type = 'summary')
+    df.game2014 = bind_rows(df.game2014, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2014$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
 }
 
-df2 <- df[!duplicated(df), ]
+length(unique(df.game2014$Player))
+
+length(unique(test$Player))
+
+#df2 <- df[!duplicated(df), ]
 
 
-MLS.Player.Game.2014 <- df
+MLS.Player.Game.2014 <-df.game2014
 
 clipr::write_clip(MLS.Player.Game.2014)
 
@@ -242,6 +250,8 @@ mls_2015_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2015/2015-Maj
 
 mls_2015.Players <- fb_team_player_stats(team_urls = mls_2015_team_urls, stat_type= 'standard')
 
+mls_2015.Players <- mls_2015.Players %>% drop_na(Min_Playing_Time)
+
 avector2015 <- as.vector(mls_2015.Players['PlayerURL'])
 
 avector2015$PlayerURL[1]
@@ -252,8 +262,42 @@ df.game2015 = data.frame()
 for (i in 1:length(avector2015$PlayerURL)) {
   output = fb_player_match_logs(avector2015$PlayerURL[i],season_end_year = 2015, stat_type = 'summary')
   
-  df.game2015 = rbind(df.game2015,output)
+  df.game2015 = bind_rows(df.game2015,output)
+  Sys.sleep(1)
 }
+
+for (i in 1:length(avector2015$PlayerURL)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2015$PlayerURL[i], season_end_year = 2015, stat_type = 'summary')
+    df.game2015 = bind_rows(df.game2015, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2015$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+mls_2015.Players
+
+df.game2015
+
+mls_2015.Players
+length(unique(mls_2015.Players$Player))
+
+length(unique(df.game2015$Player))
+
+
+test <- c(unique(df.game2015$Player))
+
+test2 <- c(mls_2015.Players$Player)
+
+identical(test2, test) 
+
+test3 <- test2 %in% test
+
+which(cumsum(test3) & rev(cumsum(rev(test3))) & !test3)
+
+match(test,test2)
 
 MLS.Player.Game.2015 <- df.game2015
 
@@ -271,3 +315,550 @@ for (i in 1:length(avector2015$PlayerURL)) {
 MLS.Player.Season.2015 <- df.season2015
 
 clipr::write_clip(MLS.Player.Season.2015)
+
+
+#2016 MLS Players Game Breakdown. 
+mls_2016_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2016/2016-Major-League-Soccer-Stats")
+
+mls_2016.Players <- fb_team_player_stats(team_urls = mls_2016_team_urls, stat_type= 'standard')
+
+mls_2016.Players <- mls_2016.Players %>% drop_na(Min_Playing_Time)
+
+avector2016 <- as.vector(mls_2016.Players['PlayerURL'])
+
+avector2016$PlayerURL[1]
+
+df.game2016 = data.frame()
+
+for (i in 1:length(avector2016$PlayerURL)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2016$PlayerURL[i], season_end_year = 2016, stat_type = 'summary')
+    df.game2016 = bind_rows(df.game2016, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2016$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+length(unique(mls_2016.Players$Player))
+
+length(unique(df.season2016$player_name))
+
+
+MLS.Player.Game.2016 <- df.game2016
+
+clipr::write_clip(MLS.Player.Game.2016)
+
+#2016 MLS Season Breakdown 
+df.season2016 = data.frame()
+
+# for (i in 1:length(avector2016$PlayerURL)) {
+#   output = fb_player_season_stats(player_url = avector2016$PlayerURL[i], stat_type = "playing_time")
+#   
+#   df.season2016 = bind_rows(df.season2016,output)
+# }
+
+for (i in 1:length(avector2016$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2016$PlayerURL[i], stat_type = "playing_time")
+    df.season2016 = bind_rows(df.season2016, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2016$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+MLS.Player.Season.2016 <- df.season2016
+
+clipr::write_clip(MLS.Player.Season.2016)
+
+
+#clipr::write_clip(MLS.Player.Season.2015)
+
+#2017 MLS Players Game Breakdown. 
+mls_2017_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2017/2017-Major-League-Soccer-Stats")
+
+mls_2017.Players <- fb_team_player_stats(team_urls = mls_2017_team_urls, stat_type= 'standard')
+
+mls_2017.Players <- mls_2017.Players %>% drop_na(Min_Playing_Time)
+
+avector2017 <- as.vector(unique(mls_2017.Players['PlayerURL']))
+
+avector2017$PlayerURL[1]
+
+avector2017part2 <- avector2017$PlayerURL[575:588]
+
+df.game2017 = data.frame()
+
+for (i in 1:length(avector2017part2)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2017part2[i], season_end_year = 2017, stat_type = 'summary')
+    df.game2017 = bind_rows(df.game2017, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2017part2[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+
+length(unique(mls_2017.Players))
+
+length(unique(df.game2017$Player))
+
+df.game2017 <- df.game2017[!duplicated(df.game2017), ]
+
+
+MLS.Player.Game.2017 <- df.game2017
+
+clipr::write_clip(MLS.Player.Game.2017)
+
+#2016 MLS Season Breakdown 
+df.season2017 = data.frame()
+
+# for (i in 1:length(avector2016$PlayerURL)) {
+#   output = fb_player_season_stats(player_url = avector2016$PlayerURL[i], stat_type = "playing_time")
+#   
+#   df.season2016 = bind_rows(df.season2016,output)
+# }
+
+for (i in 1:length(avector2017$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2017$PlayerURL[i], stat_type = "playing_time")
+    df.season2017 = bind_rows(df.season2017, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2017$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+MLS.Player.Season.2017 <- df.season2017
+
+clipr::write_clip(MLS.Player.Season.2017)
+
+length(unique(mls_2017.Players))
+
+length(unique(df.season2017$player_name))
+
+
+#2018 MLS Players Game Breakdown. 
+mls_2018_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2018/2018-Major-League-Soccer-Stats")
+
+mls_2018.Players <- fb_team_player_stats(team_urls = mls_2018_team_urls, stat_type= 'standard')
+
+mls_2018.Players <- mls_2018.Players %>% drop_na(Min_Playing_Time)
+
+avector2018 <- as.vector(unique(mls_2018.Players['PlayerURL']))
+
+avector2018$PlayerURL[1]
+
+df.game2018 = data.frame()
+
+for (i in 1:length(avector2018$PlayerURL)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2018$PlayerURL[i], season_end_year = 2018, stat_type = 'summary')
+    df.game2018 = bind_rows(df.game2018, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2018$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+length(unique(mls_2018.Players$Player))
+
+length(unique(df.game2018$Player))
+
+
+MLS.Player.Game.2018 <- df.game2018
+
+clipr::write_clip(MLS.Player.Game.2018)
+
+df.season2018 = data.frame()
+
+
+for (i in 1:length(avector2018$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2018$PlayerURL[i], stat_type = "playing_time")
+    df.season2018 = bind_rows(df.season2018, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2018$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+length(unique(mls_2018.Players$Player))
+
+length(unique(df.season2018$player_name))
+
+clipr::write_clip(df.season2018)
+
+
+#2019 MLS Players Game Breakdown. 
+mls_2019_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2019/2019-Major-League-Soccer-Stats")
+
+mls_2019.Players <- fb_team_player_stats(team_urls = mls_2019_team_urls, stat_type= 'standard')
+
+mls_2019.Players <- mls_2019.Players %>% drop_na(Min_Playing_Time)
+
+avector2019 <- as.vector(unique(mls_2019.Players['PlayerURL']))
+
+avector2019$PlayerURL[1]
+
+avector2019part1 <- avector2019$PlayerURL[1:311]
+
+avector2019part2 <- avector2019$PlayerURL[312:623]
+
+df.game2019 = data.frame()
+
+for (i in 1:length(avector2019part1)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2019part1[i], season_end_year = 2019, stat_type = 'summary')
+    df.game2019 = bind_rows(df.game2019, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2019part1[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+for (i in 1:length(avector2019part2)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2019part2[i], season_end_year = 2019, stat_type = 'summary')
+    df.game2019 = bind_rows(df.game2019, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2019part2[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+df
+
+length(unique(mls_2019.Players$Player))
+
+length(unique(df.game2019$Player))
+
+
+MLS.Player.Game.2019 <- df.game2019
+
+clipr::write_clip(MLS.Player.Game.2019)
+
+df.season2019 = data.frame()
+
+for (i in 1:length(avector2019$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2019$PlayerURL[i], stat_type = "playing_time")
+    df.season2019 = bind_rows(df.season2019, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2019$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+length(unique(mls_2019.Players$Player))
+
+length(unique(df.season2019$player_name))
+
+clipr::write_clip(df.season2019)
+
+#2020 MLS Players Game Breakdown. 
+mls_2020_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2020/2020-Major-League-Soccer-Stats")
+
+mls_2020.Players <- fb_team_player_stats(team_urls = mls_2020_team_urls, stat_type= 'standard')
+
+mls_2020.Players <- mls_2020.Players %>% drop_na(Min_Playing_Time)
+
+avector2020 <- as.vector(unique(mls_2020.Players['PlayerURL']))
+
+avector2020$PlayerURL[1]
+
+avector2020part1 <- avector2020$PlayerURL[1:334]
+
+avector2020part2 <- avector2020$PlayerURL[335:668]
+
+df.game2020 = data.frame()
+
+for (i in 1:length(avector2020part1)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2020part1[i], season_end_year = 2020, stat_type = 'summary')
+    df.game2020 = bind_rows(df.game2020, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2020part1[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+for (i in 1:length(avector2020part2)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2020part2[i], season_end_year = 2020, stat_type = 'summary')
+    df.game2020 = bind_rows(df.game2020, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2020part2[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+df
+
+length(unique(mls_2020.Players$Player))
+
+length(unique(df.game2020$Player))
+
+
+MLS.Player.Game.2020 <- df.game2020
+
+clipr::write_clip(MLS.Player.Game.2020)
+
+df.season2020 = data.frame()
+
+for (i in 1:length(avector2020$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2020$PlayerURL[i], stat_type = "playing_time")
+    df.season2020 = bind_rows(df.season2020, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2020$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+length(unique(mls_2020.Players$Player))
+
+length(unique(df.season2020$player_name))
+
+clipr::write_clip(df.season2020)
+
+ederson_summary <- fb_player_match_logs("https://fbref.com/en/players/b76e1394/Pedro-Gallese", season_end_year = 2020, stat_type = 'summary')
+
+ederson_summary2 <- fb_player_match_logs("https://fbref.com/en/players/7e1dbecf/Andres-Perea", season_end_year = 2020, stat_type = 'summary')
+
+clipr::write_clip(ederson_summary)
+clipr::write_clip(ederson_summary2)
+
+#2021 MLS Players Game Breakdown. 
+mls_2021_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2021/2021-Major-League-Soccer-Stats")
+
+mls_2021.Players <- fb_team_player_stats(team_urls = mls_2021_team_urls, stat_type= 'standard')
+
+mls_2021.Players <- mls_2021.Players %>% drop_na(Min_Playing_Time)
+
+avector2021 <- as.vector(unique(mls_2021.Players['PlayerURL']))
+
+avector2021$PlayerURL[1]
+
+avector2021part1 <- avector2021$PlayerURL[1:365]
+
+avector2021part2 <- avector2021$PlayerURL[366:730]
+
+df.game2021 = data.frame()
+
+for (i in 1:length(avector2021part1)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2021part1[i], season_end_year = 2021, stat_type = 'summary')
+    df.game2021 = bind_rows(df.game2021, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2021part1[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+for (i in 1:length(avector2021part2)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2021part2[i], season_end_year = 2021, stat_type = 'summary')
+    df.game2021 = bind_rows(df.game2021, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2021part2[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+ederson_summary <- fb_player_match_logs("https://fbref.com/en/players/ec730d9a/Russell-Canouse", season_end_year = 2021, stat_type = 'summary')
+ederson_summary2 <- fb_player_match_logs("https://fbref.com/en/players/1bb2acce/Paul-Arriola", season_end_year = 2021, stat_type = 'summary')
+
+length(unique(mls_2021.Players$Player))
+
+length(unique(df.game2021$Player))
+
+
+MLS.Player.Game.2021 <- df.game2021
+
+clipr::write_clip(MLS.Player.Game.2021)
+
+df.season2021 = data.frame()
+
+for (i in 1:length(avector2021$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2021$PlayerURL[i], stat_type = "playing_time")
+    df.season2021 = bind_rows(df.season2021, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2021$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+length(unique(mls_2021.Players$Player))
+
+length(unique(df.season2021$player_name))
+
+clipr::write_clip(df.season2021)
+
+#2022 MLS Players Game Breakdown. 
+mls_2022_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2022/2022-Major-League-Soccer-Stats")
+
+mls_2022.Players <- fb_team_player_stats(team_urls = mls_2022_team_urls, stat_type= 'standard')
+
+mls_2022.Players <- mls_2022.Players %>% drop_na(Min_Playing_Time)
+
+avector2022 <- as.vector(unique(mls_2022.Players['PlayerURL']))
+
+avector2022$PlayerURL[1]
+
+avector2022part1 <- avector2022$PlayerURL[1:385]
+
+avector2022part2 <- avector2022$PlayerURL[386:770]
+
+df.game2022 = data.frame()
+
+for (i in 1:length(avector2022part1)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2022part1[i], season_end_year = 2022, stat_type = 'summary')
+    df.game2022 = bind_rows(df.game2022, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2022part1[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+for (i in 1:length(avector2022part2)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2022part2[i], season_end_year = 2022, stat_type = 'summary')
+    df.game2022 = bind_rows(df.game2022, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2022part2[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+ederson_summary <- fb_player_match_logs("https://fbref.com/en/players/ec730d9a/Russell-Canouse", season_end_year = 2022, stat_type = 'summary')
+ederson_summary2 <- fb_player_match_logs("https://fbref.com/en/players/1bb2acce/Paul-Arriola", season_end_year = 2022, stat_type = 'summary')
+
+length(unique(mls_2022.Players$Player))
+
+length(unique(df.game2022$Player))
+
+
+MLS.Player.Game.2022 <- df.game2022
+
+clipr::write_clip(MLS.Player.Game.2022)
+
+df.season2022 = data.frame()
+
+for (i in 1:length(avector2022$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2022$PlayerURL[i], stat_type = "playing_time")
+    df.season2022 = bind_rows(df.season2022, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2022$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+length(unique(mls_2022.Players$Player))
+
+length(unique(df.season2022$player_name))
+
+clipr::write_clip(df.season2022)
+
+#2023 MLS Players Game Breakdown. 
+mls_2023_team_urls <- fb_teams_urls("https://fbref.com/en/comps/22/2023/2023-Major-League-Soccer-Stats")
+
+mls_2023.Players <- fb_team_player_stats(team_urls = mls_2023_team_urls, stat_type= 'standard')
+
+mls_2023.Players <- mls_2023.Players %>% drop_na(Min_Playing_Time)
+
+avector2023 <- as.vector(unique(mls_2023.Players['PlayerURL']))
+
+avector2023$PlayerURL[1]
+
+avector2023part1 <- avector2023$PlayerURL[1:413]
+
+avector2023part2 <- avector2023$PlayerURL[414:826]
+
+df.game2023 = data.frame()
+
+for (i in 1:length(avector2023part1)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2023part1[i], season_end_year = 2023, stat_type = 'summary')
+    df.game2023 = bind_rows(df.game2023, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2023part1[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+
+for (i in 1:length(avector2023part2)) {
+  tryCatch({
+    output = fb_player_match_logs(avector2023part2[i], season_end_year = 2023, stat_type = 'summary')
+    df.game2023 = bind_rows(df.game2023, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2023part2[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+#ederson_summary <- fb_player_match_logs("https://fbref.com/en/players/73fdc4c2/Ian-Harkes", season_end_year = 2023, stat_type = 'passing')
+#ederson_summary2 <- fb_player_match_logs("https://fbref.com/en/players/1bb2acce/Paul-Arriola", season_end_year = 2023, stat_type = 'summary')
+
+length(unique(mls_2023.Players$Player))
+
+length(unique(df.game2023$Player))
+
+
+MLS.Player.Game.2023 <- df.game2023
+
+clipr::write_clip(MLS.Player.Game.2023)
+
+df.season2023 = data.frame()
+
+for (i in 1:length(avector2023$PlayerURL)) {
+  tryCatch({
+    output = fb_player_season_stats(player_url = avector2023$PlayerURL[i], stat_type = "playing_time")
+    df.season2023 = bind_rows(df.season2023, output)
+  }, error = function(e) {
+    print(paste("Error for URL:", avector2023$PlayerURL[i]))
+    print(e)
+  })
+  Sys.sleep(1)
+}
+
+length(unique(mls_2023.Players$Player))
+
+length(unique(df.season2023$player_name))
+
+clipr::write_clip(df.season2023)
